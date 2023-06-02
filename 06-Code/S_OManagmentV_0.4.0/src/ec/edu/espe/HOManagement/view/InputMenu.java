d2e82e78411dfea4682c46c090ff0cbb5a21e8f8
Edison Verdesoto <edisondamian12@gmail.com>
2023-06-02 11:40:26 -0500

    update code

--- 06-Code/S_OManagmentV_0.4.0/src/ec/edu/espe/HOManagement/view/InputMenu.java
+++ 06-Code/S_OManagmentV_0.4.0/src/ec/edu/espe/HOManagement/view/InputMenu.java
@@ -1,16 +1,21 @@
-package ec.edu.espe.HOManagement.model;
+package ec.edu.espe.HOManagement.view;
+
+import ec.edu.espe.HOManagement.model.ProductItem;
+import ec.edu.espe.HOManagement.model.Report;
 import java.util.Scanner;
 
 /**
  *
- * @author Mateo Quishpe, CodeWars, DCCO-ESPE
+ * @author Code Warriors, DCCO-ESPE
  */
 
-public class Menu {
-    public static void main(String[] args) {
+
+
+public class InputMenu {
+    public static void menu() {
         try (Scanner scanner = new Scanner(System.in)) {
-            int opcion;
-            
+            int option;
+
             do {
                 System.out.println("----- Menú -----");
                 System.out.println("1. Crear usuarios");
@@ -20,43 +25,41 @@ public class Menu {
                 System.out.println("5. Contactos de proveedores");
                 System.out.println("6. Salir");
                 System.out.print("Ingrese la opción: ");
-                opcion = scanner.nextInt();
-                
-                switch (opcion) {
-                    case 1 -> crearUsuarios();
-                    case 2 -> ingresarProductos();
-                    case 3 -> venderProductos();
-                    case 4 -> verReporteVentas();
-                    case 5 -> contactosProveedores();
+                option = scanner.nextInt();
+
+                switch (option) {
+                    case 1 -> createUsers();
+                    case 2 -> enterProducts();
+                    case 3 -> sellProducts();
+                    case 4 -> viewSalesReport();
+                    case 5 -> supplierContacts();
                     case 6 -> System.out.println("Saliendo del programa...");
                     default -> System.out.println("Opción inválida");
                 }
-            } while (opcion != 6);
+            } while (option != 6);
         }
     }
 
-    public static void crearUsuarios() {
-        // Lógica para crear usuarios
+    public static void createUsers() {
         System.out.println("Se han creado los usuarios");
     }
 
-    public static void ingresarProductos() {
-        // Lógica para ingresar productos
+    public static void enterProducts() {
+        ProductItem.item();
         System.out.println("Se han ingresado los productos");
     }
 
-    public static void venderProductos() {
-        // Lógica para vender productos
+    public static void sellProducts() {
+      
         System.out.println("Se han vendido los productos");
     }
-
-    public static void verReporteVentas() {
-        // Lógica para ver el reporte de ventas
+    public static void viewSalesReport() {
+        Report.generateReport(ProductItem.item()); 
         System.out.println("Se muestra el reporte de ventas");
-    }
+}
 
-    public static void contactosProveedores() {
-        // Lógica para mostrar los contactos de proveedores
+    public static void supplierContacts() {
         System.out.println("Se muestran los contactos de proveedores");
     }
 }
+
