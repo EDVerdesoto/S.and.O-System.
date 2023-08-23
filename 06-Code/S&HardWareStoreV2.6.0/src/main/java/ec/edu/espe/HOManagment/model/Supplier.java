337d7bf5689a3c39f2fafd3416d203c6040ee060
Edison Verdesoto <edisondamian12@gmail.com>
2023-08-22 20:26:42 -0500

    upload image to the project

---
+++ 06-Code/S&HardWareStoreV2.6.0/src/main/java/ec/edu/espe/HOManagment/model/Supplier.java
@@ -0,0 +1,103 @@
+
+package ec.edu.espe.HOManagment.model;
+
+/**
+ *
+ * @author Code Warriors, DCCO-ESPE
+ */
+public class Supplier {
+
+    private String name;
+    private String number;
+    private double pendingPayment;
+    private String address;
+    private String invoiceNumber;
+
+    public Supplier() {
+    }
+
+    public Supplier(String name, String number, double pendingPayment, String address, String invoiceNumber) {
+        this.name = name;
+        this.number = number;
+        this.pendingPayment = pendingPayment;
+        this.address = address;
+        this.invoiceNumber= invoiceNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" + "name=" + name + ", number=" + number + ", pendingPayment=" + pendingPayment  + ", address=" + address + ",Invoice Number="+ invoiceNumber+'}';
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the pendingPayment
+     */
+    public double getPendingPayment() {
+        return pendingPayment;
+    }
+
+    /**
+     * @param pendingPayment the pendingPayment to set
+     */
+    public void setPendingPayment(double pendingPayment) {
+        this.pendingPayment = pendingPayment;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    /**
+     * @return the invoiceNumber
+     */
+    public String getinvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    /**
+     * 
+     * @param invoiceNumber the invoiceNumber to set
+     */
+    public void setinvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+}
